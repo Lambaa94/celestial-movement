ab7b5f26be137c7b1fe7ccfb52f6cd49f4fbccc7
James Lamb <jameslamb13@gmail.com>
2023-02-24 15:58:28 -0500

    adding some elements into Navbar.js

--- celestial-movenment/src/components/Navbar/Navbar.js
+++ celestial-movenment/src/components/Navbar/Navbar.js
@@ -1,1 +1,14 @@
+import React from "react";
+import {Navbar} from 'react-bootstrap';
+import "./Navbar.css";
 
+
+function Navtop() {
+
+    return (
+<Navbar></Navbar>
+
+    )
+}
+
+export default Navtop
